6b8f07a582280a045e98cb5ad11eb5d2fbae6f22
mdkhatami <mdkhatami@gmail.com>
2023-11-13 07:32:15 +0100

    sorting algorithm

---
+++ sorting.py
@@ -0,0 +1,15 @@
+# Python script for sorting a list of numbers
+
+# Sample list of numbers
+numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+
+# Sorting the list in ascending order
+sorted_numbers = sorted(numbers)
+
+# Printing the original and sorted lists
+print("Original list:", numbers)
+print("Sorted list:", sorted_numbers)
+
+# To sort in descending order, uncomment the following line:
+# sorted_numbers_desc = sorted(numbers, reverse=True)
+# print("Sorted list (descending):", sorted_numbers_desc)
